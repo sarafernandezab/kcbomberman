e6b5923df9846eb6a83061e44ab2231d45213cd2
cahadras <cahadras@07018c45-834e-0410-9474-9dd5cf394506>
2008-05-31 16:07:49 +0000

    KeyListener now works for the frame. This was done using a custom EventQueue.

--- trunk/bomberman/client/gui/MainFrame.java
+++ trunk/bomberman/client/gui/MainFrame.java
@@ -6,6 +6,8 @@
 package bomberman.client.gui;
 
 import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
