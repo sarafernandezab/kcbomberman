ed95f59a927ef8d3a208f517321631e8a5f83d4e
cahadras <cahadras@07018c45-834e-0410-9474-9dd5cf394506>
2008-06-16 12:14:20 +0000

    Server GUI is now showing per default.

--- trunk/bomberman/server/Logger.java
+++ trunk/bomberman/server/Logger.java
@@ -19,35 +19,10 @@
 
 package bomberman.server;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+/**
+ *
+ * @author chris
+ */
+public class Logger {
 
-public class ServerThread extends Thread
-{
-  public ServerThread(boolean daemon)
-  {
-    setDaemon(daemon);
-  }
-  
-  @Override
-  public void run()
-  {
-    try
-    {
-      Server server = new Server();
-      
-      // Erzeuge neue lokale Registry
-      Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-      
-      // Binde Server an Namen
-      registry.rebind("Server", server);
-      
-      System.out.println("Bombermanserver bereit ...");
-    }
-    catch(Exception ex)
-    {
-      ex.printStackTrace();
-      System.exit(1);
-    }
-  }
 }
