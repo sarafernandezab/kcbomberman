7cace881c147527f7294344aafd6e8c733e05a97
cahadras <cahadras@07018c45-834e-0410-9474-9dd5cf394506>
2008-06-16 08:19:43 +0000

    Added Playground initialization code from old Bomberman.

--- trunk/bomberman/server/Playground.java
+++ trunk/bomberman/server/Playground.java
@@ -20,18 +20,72 @@
 package bomberman.server;
 
 import bomberman.server.api.Element;
+import java.io.Serializable;
 
 /**
- * Playground.
+ * Logical Playground where the Server manages a game.
  * @author Kai Ritterbusch
  * @author Christian Lins
  */
-public class Playground 
+public class Playground implements Serializable
 {
+
   private Element[][] matrix = null;
-  
+
   public Playground(int cols, int rows)
   {
     this.matrix = new Element[cols][rows];
+
+    // Initialize the playground
+    for (int x = 0; x < cols; x++)
+    {
+      for (int y = 0; y < rows; y++)
+      {
+        matrix[x][y] = null;
+
+        // Solid borders
+        if ((x == 0) || (x == cols - 1) || (y == 0) || (y == rows - 1))
+        {
+          this.matrix[x][y] = new Wall(false); // A solid wall
+        }
+        // Player starting points
+        else if ((x == 1 && (y == 1 || y == 2)) || (x == 2 && y == 1) || // Links oben
+                (x == cols - 2 && (y == 1 || y == 2)) || (x == cols - 3 && y == 1) || // Rechts oben
+                (x == 1 && (y == rows - 2 || y == rows - 3)) || (x == 2 && y == rows - 2) || // Links unten
+                (x == cols - 2 && (y == rows - 2 || y == rows - 3)) || (x == cols - 3 && y == rows - 3) // Rechts unten
+                )
+        {
+          continue;
+        }
+        // Solid walls within
+        else if ((y % 2 == 0) && (x % 2 == 0))
+        {
+          this.matrix[x][y] = new Wall(false); // Solid wall
+        }
+        else
+        {
+          if (Math.random() < 0.2) // 20% of the area should be empty
+          {
+            continue;
+          }
+          else
+          {
+            matrix[x][y] = new Wall(true); // Exploadable wall
+ 
+            // Extras are placed later when a Wall explodes.
+          }
+        }
+      }
+    }
+  }
+
+  public Element getElement(int x, int y)
+  {
+    return this.matrix[x][y];
+  }
+
+  public void setElement(int x, int y, Element e)
+  {
+    this.matrix[x][y] = e;
   }
 }
