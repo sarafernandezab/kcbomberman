58429125ed2f9c43857e8f702fdf17f6d72b9355
kai.ritterbusch <kai.ritterbusch@07018c45-834e-0410-9474-9dd5cf394506>
2008-06-19 11:09:29 +0000

    insert Extras

---
+++ trunk/bomberman/server/Extra.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bomberman.server;
+
+import bomberman.server.api.Element;
+
+/**
+ *
+ * @author Kai Ritterbusch (kai.ritterbusch@fh-osnabrueck.de)
+ */
+abstract class Extra extends Element
+{ 
+  public Extra(int x, int y)
+  {
+    super(x, y);
+  }
+  
+ 
+}
