ec0618ea555c854fecebc77a0f660f11bfcd4248
kai.ritterbusch <kai.ritterbusch@07018c45-834e-0410-9474-9dd5cf394506>
2008-06-19 11:09:29 +0000

    insert Extras

---
+++ bomberman/server/ExtraDistance.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bomberman.server;
+
+/**
+ *
+ * @author kai
+ */
+public class ExtraDistance extends Extra
+{
+  public static final String IMAGE_FILENAME = "resource/gfx/bombDistance.png";
+  
+  public ExtraDistance(int x, int y)
+  {
+    super(x,y);
+  }
+  
+  public String getImageFilename()
+  {
+    return IMAGE_FILENAME;
+  }
+}
