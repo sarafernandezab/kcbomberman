0be4823187ebae1eaa5cbabbdef78f4185545534
cahadras <cahadras@07018c45-834e-0410-9474-9dd5cf394506>
2008-07-01 08:46:42 +0000

    Added bomberman.server.rmi package description.

--- bomberman/server/rmi/RMIClientSocket.java
+++ bomberman/server/rmi/RMIClientSocket.java
@@ -1,24 +1,40 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  KC Bomberman
+ *  Copyright 2008 Christian Lins <christian.lins@web.de>
+ *  Copyright 2008 Kai Ritterbusch <kai.ritterbusch@googlemail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package bomberman.server.rmi;
 
+import bomberman.Main;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 /**
- *
- * @author chris
+ * A custom socket with additional logging support.
+ * @author Christian Lins (christian.lins@web.de)
  */
 public class RMIClientSocket extends Socket
 {
   public RMIClientSocket(String host, int port) throws IOException, UnknownHostException
   {
-    System.out.println("DEBUG: Connecting to " + host + ":" + port);
+    if(Main.Debugging)
+      System.out.println("DEBUG: Connecting to " + host + ":" + port);
     connect(new InetSocketAddress(host, port));
   }
 }
